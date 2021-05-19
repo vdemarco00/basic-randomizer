8a0ce29d4c874dc203924228345256c08c1a57c8
Vinny DeMarco <vinny401@gmail.com>
2021-05-19 17:54:39 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-test';
+  content: any;
+  restList: string[];
+
+
+  constructor()
+  {
+    this.restList = ["Longhorn","Kyoto","Davio's","Cafe Assisi","Cibo Matto","Trattoria Della Nonna","Sabatino's","Trattoria Romana","Fresh Catch","Canova"];
+  }
+  
+  ngOnInit(): void {
+    this.content = document.getElementById("body");
+    
+  }
+;
+
+  public GenerateRandom(): void {
+    var randIndex = Math.floor(Math.random() * this.restList.length);
+    console.log(randIndex);
+    console.log(this.restList[randIndex]);
+    this.content.textContent = this.restList[randIndex];
+  }
+
 }
